8631d1b7692eee49ea55be77ff18b110404c790f
unknown <sonypulickeel@gmail.com>
2020-02-02 21:25:51 +0530

    speak and loader

--- src/App.js
+++ src/App.js
@@ -1,53 +1,117 @@
 import React, { Component } from "react";
 import Speech from 'speak-tts'
+import axios from 'axios';
+import 'react-dropzone-uploader/dist/styles.css'
+import Dropzone from 'react-dropzone-uploader'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from 'react-loader-spinner'
 // import logo from './logo.svg';
 import "./App.css";
 import Particles from "react-particles-js";
-import { Upload, Icon, message } from "antd";
 
 const speech = new Speech()
-const { Dragger } = Upload;
-const props = {
-  name: "file",
-  multiple: true,
-  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== "uploading") {
-      console.log(info.file, info.fileList);
-    }
-    if (status === "done") {
-      message.success(`${info.file.name} file uploaded successfully.`);
-      const url = 'http://audiot.herokuapp.com/api/translate/'+info.file.name
-      const formData = new FormData();
-      formData.append('file', info.file);
-      fetch(url, {
-        method: 'POST',
-        body: formData
-      })
-      .then(data => console.log("ddddd",data))
-      .catch(err => console.log("errrr",err));
-      console.log("fgfgdfgdfgdfg",info.file)
-      
-    } else if (status === "error") {
-      message.error(`${info.file.name} file upload failed.`);
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      spanishTrans:"",
+      hindiTrans:"",
+      frenchTrans:"",
+      italianTrans:"",
+      arabicTrans:"",
+      loader:false
     }
   }
-};
-class App extends Component {
   SpanishTrans(){
-    console.log("sdfsdf")
+    speech.setLanguage('es-ES')
     speech.speak({
-      text: 'Hello, how are you today ?',
+      text: this.state.spanishTrans,
   }).then(() => {
       console.log("Success !")
   }).catch(e => {
       console.error("An error occurred :", e)
   })
   }
+  HindiTrans(){
+    speech.setLanguage('hi-IN')
+    speech.speak({
+      text: this.state.hindiTrans,
+  }).then(() => {
+      console.log("Success !")
+  }).catch(e => {
+      console.error("An error occurred :", e)
+  })
+  }
+  FrenchTrans(){
+    speech.setLanguage('fr-FR')
+    speech.speak({
+      text: this.state.frenchTrans,
+  }).then(() => {
+      console.log("Success !")
+  }).catch(e => {
+      console.error("An error occurred :", e)
+  })
+  }
+  ItalianTrans(){
+    speech.setLanguage('it-IT')
+    speech.speak({
+      text: this.state.italianTrans,
+  }).then(() => {
+      console.log("Success !")
+  }).catch(e => {
+      console.error("An error occurred :", e)
+  })
+  }
+  ArabicTrans(){
+    console.log("bbbbbbbbbb",this.state.arabicTrans)
+    speech.setLanguage('ar-SA')
+    speech.speak({
+      text: this.state.arabicTrans,
+  }).then(() => {
+      console.log("Success !")
+  }).catch(e => {
+      console.error("An error occurred :", e)
+  })
+  }
+  getUploadParams = ({ meta }) => { 
+    const url = 'https://httpbin.org/post'
+    return { 
+    
+    url: url, meta: { fileUrl: `${url}/${encodeURIComponent(meta.name)}` },type:"audio/wav" } }
+  
+  // called every time a file's `status` changes
+  handleChangeStatus = ({ meta, file }, status) => {
+    // this.setState({loader:true})
+    this.state.loader=true
+    if(status=="done"){
+      const url = 'http://audiot.herokuapp.com/api/translate/AUD-20200201-WA0002'
+        console.log("fgfghfssssssss",file)
+        axios.post(url,file,{
+          headers: { "content-type": "audio/wav"}
+      })
+        .then(res => {
+          console.log("fdgdgdgdf",res.data.translated.ar)
+          this.state.loader=false
+          this.state.spanishTrans=res.data.translated.es
+          this.state.hindiTrans=res.data.translated.hi
+          this.state.frenchTrans=res.data.translated.fr
+          this.state.italianTrans=res.data.translated.la
+          this.state.arabicTrans=res.data.translated.ar
+        })
+    }
+     console.log(status, meta, file) }
+  
+  // receives array of files that are done uploading when submit button is clicked
+  handleSubmit = (files, allFiles) => {
+    console.log(files.map(f => f.meta))
+    allFiles.forEach(f => f.remove())
+  }
+
   render() {
     return (
       <div className="App">
+      
         <Particles
           params={{
             particles: {
@@ -69,9 +133,17 @@ class App extends Component {
           }}
         />
         <div className="container ">
+        <Loader
+         visible={this.state.loader}
+         type="Audio"
+         color="#00BFFF"
+         height={100}
+         width={100}
+
+      />
           <div className="col-md-12 dragContainer">
             <h1 className="text-center text-white">Audio Translator</h1>
-            <Dragger {...props}>
+            {/* <Dragger {...props}>
               <p className="ant-upload-drag-icon">
                 <Icon type="inbox" />
               </p>
@@ -79,10 +151,15 @@ class App extends Component {
                 Click or drag audio file to translate
               </p>
               <p className="ant-upload-hint">
-                {/* Support for a single or bulk upload. Strictly prohibit from
-                uploading company data or other band files */}
+               
               </p>
-            </Dragger>
+            </Dragger> */}
+            <Dropzone
+      getUploadParams={this.getUploadParams}
+      onChangeStatus={this.handleChangeStatus}
+      onSubmit={this.handleSubmit}
+      accept="image/*,audio/*,video/*"
+    />
           </div>
           <div className="col-md-12">
             <h2 className="text-center text-white mT30">
@@ -97,6 +174,7 @@ class App extends Component {
               class="clickableimage"
               src="https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/de.png"
               height="100"
+              onClick={this.HindiTrans.bind(this)}
             />
             <br />
             German
@@ -120,6 +198,7 @@ class App extends Component {
               class="clickableimage"
               src="https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/fr.png"
               height="100"
+              onClick={this.FrenchTrans.bind(this)}
             />
             <br />
             French
@@ -132,6 +211,7 @@ class App extends Component {
               class="clickableimage"
               src="https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/it.png"
               height="100"
+              onClick={this.ItalianTrans.bind(this)}
             />
             <br />
             Italian
@@ -144,6 +224,7 @@ class App extends Component {
               class="clickableimage"
               src="https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/sv.png"
               height="100"
+              onClick={this.ArabicTrans.bind(this)}
             />
             <br />
             Swedish
