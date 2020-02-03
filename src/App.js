5803ee02bdb7bf7bced6412ba6fced24e2d2b9d3
Kiran Correya <kiran.c@xtrastaff.com>
2020-02-03 11:59:46 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,117 +1,142 @@
 import React, { Component } from "react";
-import Speech from 'speak-tts'
-import axios from 'axios';
-import 'react-dropzone-uploader/dist/styles.css'
-import Dropzone from 'react-dropzone-uploader'
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
-import Loader from 'react-loader-spinner'
+import Speech from "speak-tts";
+import axios from "axios";
+import "react-dropzone-uploader/dist/styles.css";
+import Dropzone from "react-dropzone-uploader";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 // import logo from './logo.svg';
 import "./App.css";
 import Particles from "react-particles-js";
+import india from "./Assets/tn_in-flag.gif";
+import france from "./Assets/tn_fr-flag.gif";
+import italy from "./Assets/tn_it-flag.gif";
+import saudi from "./Assets/tn_sa-flag.gif";
+import spain from "./Assets/tn_sp-flag.gif";
 
-const speech = new Speech()
+const speech = new Speech();
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      spanishTrans:"",
-      hindiTrans:"",
-      frenchTrans:"",
-      italianTrans:"",
-      arabicTrans:"",
-      loader:false
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      spanishTrans: "",
+      hindiTrans: "",
+      frenchTrans: "",
+      italianTrans: "",
+      arabicTrans: "",
+      loader: false
+    };
   }
-  SpanishTrans(){
-    speech.setLanguage('es-ES')
-    speech.speak({
-      text: this.state.spanishTrans,
-  }).then(() => {
-      console.log("Success !")
-  }).catch(e => {
-      console.error("An error occurred :", e)
-  })
+  SpanishTrans() {
+    speech.setLanguage("es-ES");
+    speech
+      .speak({
+        text: this.state.spanishTrans
+      })
+      .then(() => {
+        console.log("Success !");
+      })
+      .catch(e => {
+        console.error("An error occurred :", e);
+      });
   }
-  HindiTrans(){
-    speech.setLanguage('hi-IN')
-    speech.speak({
-      text: this.state.hindiTrans,
-  }).then(() => {
-      console.log("Success !")
-  }).catch(e => {
-      console.error("An error occurred :", e)
-  })
+  HindiTrans() {
+    speech.setLanguage("hi-IN");
+    speech
+      .speak({
+        text: this.state.hindiTrans
+      })
+      .then(() => {
+        console.log("Success !");
+      })
+      .catch(e => {
+        console.error("An error occurred :", e);
+      });
   }
-  FrenchTrans(){
-    speech.setLanguage('fr-FR')
-    speech.speak({
-      text: this.state.frenchTrans,
-  }).then(() => {
-      console.log("Success !")
-  }).catch(e => {
-      console.error("An error occurred :", e)
-  })
+  FrenchTrans() {
+    speech.setLanguage("fr-FR");
+    speech
+      .speak({
+        text: this.state.frenchTrans
+      })
+      .then(() => {
+        console.log("Success !");
+      })
+      .catch(e => {
+        console.error("An error occurred :", e);
+      });
   }
-  ItalianTrans(){
-    speech.setLanguage('it-IT')
-    speech.speak({
-      text: this.state.italianTrans,
-  }).then(() => {
-      console.log("Success !")
-  }).catch(e => {
-      console.error("An error occurred :", e)
-  })
+  ItalianTrans() {
+    speech.setLanguage("it-IT");
+    speech
+      .speak({
+        text: this.state.italianTrans
+      })
+      .then(() => {
+        console.log("Success !");
+      })
+      .catch(e => {
+        console.error("An error occurred :", e);
+      });
   }
-  ArabicTrans(){
-    console.log("bbbbbbbbbb",this.state.arabicTrans)
-    speech.setLanguage('ar-SA')
-    speech.speak({
-      text: this.state.arabicTrans,
-  }).then(() => {
-      console.log("Success !")
-  }).catch(e => {
-      console.error("An error occurred :", e)
-  })
+  ArabicTrans() {
+    console.log("bbbbbbbbbb", this.state.arabicTrans);
+    speech.setLanguage("ar-SA");
+    speech
+      .speak({
+        text: this.state.arabicTrans
+      })
+      .then(() => {
+        console.log("Success !");
+      })
+      .catch(e => {
+        console.error("An error occurred :", e);
+      });
   }
-  getUploadParams = ({ meta }) => { 
-    const url = 'https://httpbin.org/post'
-    return { 
-    
-    url: url, meta: { fileUrl: `${url}/${encodeURIComponent(meta.name)}` },type:"audio/wav" } }
-  
+  getUploadParams = ({ meta }) => {
+    const url = "https://httpbin.org/post";
+    return {
+      url: url,
+      meta: { fileUrl: `${url}/${encodeURIComponent(meta.name)}` },
+      type: "audio/wav"
+    };
+  };
+
   // called every time a file's `status` changes
   handleChangeStatus = ({ meta, file }, status) => {
     // this.setState({loader:true})
-    this.state.loader=true
-    if(status=="done"){
-      const url = 'http://audiot.herokuapp.com/api/translate/AUD-20200201-WA0002'
-        console.log("fgfghfssssssss",file)
-        axios.post(url,file,{
-          headers: { "content-type": "audio/wav"}
-      })
-        .then(res => {
-          console.log("fdgdgdgdf",res.data.translated.ar)
-          this.state.loader=false
-          this.state.spanishTrans=res.data.translated.es
-          this.state.hindiTrans=res.data.translated.hi
-          this.state.frenchTrans=res.data.translated.fr
-          this.state.italianTrans=res.data.translated.la
-          this.state.arabicTrans=res.data.translated.ar
+    this.state.loader = true;
+    if (status == "done") {
+      const url =
+        "http://audiot.herokuapp.com/api/translate/AUD-20200201-WA0002";
+      console.log("fgfghfssssssss", file);
+      axios
+        .post(url, file, {
+          headers: { "content-type": "audio/wav" }
         })
+        .then(res => {
+          console.log("fdgdgdgdf", res.data.translated.ar);
+          this.state.loader = false;
+          this.state.spanishTrans = res.data.translated.es;
+          this.state.hindiTrans = res.data.translated.hi;
+          this.state.frenchTrans = res.data.translated.fr;
+          this.state.italianTrans = res.data.translated.la;
+          this.state.arabicTrans = res.data.translated.ar;
+        });
     }
-     console.log(status, meta, file) }
-  
+    console.log(status, meta, file);
+  };
+
   // receives array of files that are done uploading when submit button is clicked
   handleSubmit = (files, allFiles) => {
-    console.log(files.map(f => f.meta))
-    allFiles.forEach(f => f.remove())
-  }
+    console.log(files.map(f => f.meta));
+    allFiles.forEach(f => f.remove());
+  };
 
   render() {
     return (
       <div className="App">
-      
         <Particles
           params={{
             particles: {
@@ -133,14 +158,13 @@ class App extends Component {
           }}
         />
         <div className="container ">
-        <Loader
-         visible={this.state.loader}
-         type="Audio"
-         color="#00BFFF"
-         height={100}
-         width={100}
-
-      />
+          <Loader
+            visible={this.state.loader}
+            type="Audio"
+            color="#00BFFF"
+            height={100}
+            width={100}
+          />
           <div className="col-md-12 dragContainer">
             <h1 className="text-center text-white">Audio Translator</h1>
             {/* <Dragger {...props}>
@@ -155,11 +179,11 @@ class App extends Component {
               </p>
             </Dragger> */}
             <Dropzone
-      getUploadParams={this.getUploadParams}
-      onChangeStatus={this.handleChangeStatus}
-      onSubmit={this.handleSubmit}
-      accept="image/*,audio/*,video/*"
-    />
+              getUploadParams={this.getUploadParams}
+              onChangeStatus={this.handleChangeStatus}
+              // onSubmit={this.handleSubmit}
+              accept="image/*,audio/*,video/*"
+            />
           </div>
           <div className="col-md-12">
             <h2 className="text-center text-white mT30">
@@ -172,7 +196,7 @@ class App extends Component {
             <img
               id="target_de"
               class="clickableimage"
-              src="https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/de.png"
+              src={india}
               height="100"
               onClick={this.HindiTrans.bind(this)}
             />
@@ -185,7 +209,7 @@ class App extends Component {
             <img
               id="target_es"
               class="clickableimage"
-              src="https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/es.png"
+              src={spain}
               height="100"
               onClick={this.SpanishTrans.bind(this)}
             />
@@ -196,7 +220,7 @@ class App extends Component {
             <img
               id="target_fr"
               class="clickableimage"
-              src="https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/fr.png"
+              src={france}
               height="100"
               onClick={this.FrenchTrans.bind(this)}
             />
@@ -209,7 +233,7 @@ class App extends Component {
             <img
               id="target_it"
               class="clickableimage"
-              src="https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/it.png"
+              src={italy}
               height="100"
               onClick={this.ItalianTrans.bind(this)}
             />
@@ -222,7 +246,7 @@ class App extends Component {
             <img
               id="target_sv"
               class="clickableimage"
-              src="https://s3.amazonaws.com/tomash-us-east-1/voice-translator/graphics/flags/sv.png"
+              src={saudi}
               height="100"
               onClick={this.ArabicTrans.bind(this)}
             />
